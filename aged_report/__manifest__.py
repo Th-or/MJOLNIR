98b220adc2f9c37b39921321f915fa0e3ee99b7c
Rahul <rahulganig46@gmail.com>
2019-04-20 15:57:56 +0530

    Age

---
+++ aged_report/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'TDS Report',
+    'summary': 'TDS Report',
+    'category': 'account',
+    'version': '12.0.0.3',
+    'description': """Print and Send your Sales Order by Post""",
+    'depends': ['base', 'tds_calculation','account'],
+    'website': 'http://www.prixgen.com',
+    'data': [],
+    'auto_install': False,
+    'application': True,
+}
